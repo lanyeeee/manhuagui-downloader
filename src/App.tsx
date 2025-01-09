89550e06ec8f72637de3563b1ea97a834f02974d
lanyeeee <1210347077@qq.com>
2025-01-09 19:30:47 +0800

    feat: 引入`tauri-specta`来保证类型安全

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import { invoke } from '@tauri-apps/api/core'
 import './App.css'
 import { Button } from 'antd'
+import { commands } from './bindings.ts'
 
 function App() {
   const [greetMsg, setGreetMsg] = useState('')
@@ -10,7 +10,7 @@ function App() {
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke('greet', { name }))
+    setGreetMsg(await commands.greet(name))
   }
 
   return (
