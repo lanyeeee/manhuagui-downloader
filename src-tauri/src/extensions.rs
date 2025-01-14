43c32f6b44978018b5d95a98ffdb9fa6d74d63bc
lanyeeee <lanyeeeeee@gmail.com>
2025-01-14 12:47:50 +0800

    feat: 用户友好的网络连接超时错误提示

--- src-tauri/src/extensions.rs
+++ src-tauri/src/extensions.rs
@@ -1,4 +1,6 @@
 use anyhow::anyhow;
+use reqwest::Response;
+use reqwest_middleware::RequestBuilder;
 use scraper::error::SelectorErrorKind;
 
 pub trait AnyhowErrorToStringChain {
@@ -29,5 +31,27 @@ pub trait ToAnyhow<T> {
 impl<T> ToAnyhow<T> for Result<T, SelectorErrorKind<'_>> {
     fn to_anyhow(self) -> anyhow::Result<T> {
         self.map_err(|e| anyhow!(e.to_string()))
+    }
+}
+
+pub trait SendWithTimeoutMsg {
+    /// 发送请求并处理超时错误
+    ///
+    /// - 如果遇到超时错误，返回带有用户友好信息的错误
+    /// - 否则返回原始错误
+    async fn send_with_timeout_msg(self) -> anyhow::Result<Response>;
+}
+
+impl SendWithTimeoutMsg for RequestBuilder {
+    async fn send_with_timeout_msg(self) -> anyhow::Result<Response> {
+        self.send().await.map_err(|e| {
+            if e.is_timeout() || e.is_middleware() {
+                anyhow::Error::from(e).context(
+                    "网络连接超时，可能是未使用代理或IP被封，请使用代理或切换代理线路后重试",
+                )
+            } else {
+                anyhow::Error::from(e)
+            }
+        })
     }
 }
