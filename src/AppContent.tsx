595a1b384fce9e0c52ad0aa231784b1e6da28246
lanyeeee <1210347077@qq.com>
2025-01-31 06:04:57 +0800

    feat: 后端支持获取日志文件占用的磁盘空间

--- src/AppContent.tsx
+++ src/AppContent.tsx
@@ -51,7 +51,7 @@ function AppContent({ config, setConfig }: Props) {
   }, [config.cookie, message])
 
   async function test() {
-    const result = await commands.updateDownloadedComics()
+    const result = await commands.getLogsSize()
     console.log(result)
   }
 
