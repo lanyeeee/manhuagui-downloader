b8f5568b56586fdf63f83f90c19aad3948f49ea4
lanyeeee <lanyeeeeee@gmail.com>
2026-04-21 04:40:35 +0800

    feat: 引入react-scan

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
+import { scan } from 'react-scan'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'virtual:uno.css'
+
+scan({
+  enabled: true,
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
