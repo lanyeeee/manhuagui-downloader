ac744f899c279cf3572c34ecf256f1d1ab4a4094
lanyeeee <lanyeeeeee@gmail.com>
2026-04-26 11:54:05 +0800

    refactor: [WIP] 迁移ChapterPane至vue3

--- src/AppContent.vue
+++ src/AppContent.vue
@@ -5,7 +5,7 @@ import LoginDialog from './dialogs/LoginDialog.vue'
 import ProgressesPane from './panes/ProgressesPane/ProgressesPane.tsx'
 import { CurrentTabName } from './types.ts'
 import SearchPane from './panes/SearchPane.vue'
-import ChapterPane from './panes/ChapterPane.tsx'
+import ChapterPane from './panes/ChapterPane.vue'
 import FavoritePane from './panes/FavoritePane.vue'
 import DownloadedPane from './panes/DownloadedPane/DownloadedPane.vue'
 import LogDialog from './dialogs/LogDialog.vue'
@@ -134,9 +134,9 @@ onMounted(async () => {
         <n-tab-pane class="h-full overflow-auto p-0!" name="downloaded" tab="本地库存" display-directive="show">
           <DownloadedPane />
         </n-tab-pane>
-        <!--        <n-tab-pane class="h-full overflow-auto p-0!" name="chapter" tab="章节详情" display-directive="show">-->
-        <!--          <ChapterPane />-->
-        <!--        </n-tab-pane>-->
+        <n-tab-pane class="h-full overflow-auto p-0!" name="chapter" tab="章节详情" display-directive="show">
+          <ChapterPane />
+        </n-tab-pane>
       </n-tabs>
 
       <div class="w-1/2 overflow-auto flex flex-col">
